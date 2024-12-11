66c61c90d79aa9195858f71d33f9b446a74a080e
thamizhkayal <thamizharasan2373@gmail.com>
2024-12-11 16:53:02 +0530

    encoded url added

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const fileInp = document.querySelector("#file-inp")
 const imgEl = document.querySelector("#img")
 const frame = document.querySelector("#iframe")
 
+
+
+
 fileInp.addEventListener("change", (e) => {
     console.log(e.target)
     const file = e.target.files[0]
     const type = file.type
 
     if (file) {
-        const url = window.URL.createObjectURL(file)
+        const url = encodeURIComponent(window.URL.createObjectURL(file))
         console.log(url)
         console.log(type)
 
